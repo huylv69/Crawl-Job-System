f978546bd819e3a329b8b2a702961a0e9e1f6f0d
huylv <huylevan@admicro.vn>
2018-11-28 16:39:29 +0700

    update data company post

---
+++ TUTScrapy/ProcessData/dataCandidate.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    passwd="Vanhuy@123",
+    database="linkjob"
+)
+mycursor = mydb.cursor()
+import json
+from pprint import pprint
+
+with open('candidate.json') as f:
+    data = json.load(f)
+
+with open('dictcareer.json', encoding='utf8') as f:
+    dictCareer = json.load(f)
+
+# # insert company
+# for company in dataCompany:
+#    sqlCompany = "INSERT INTO company (idcompany, email, password, activated, name , address, about, website, scale) value (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+#    pprint(company["about"])
+#    mycursor.execute(sqlCompany, (company["id"],company["email"],company["password"],True,company["name"],company["address"], company["about"],company["website"],company["scale"]))
+# mydb.commit()
